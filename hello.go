5b82633e2d04ecd3f68f478afdad218e1101cce8
PatrickWynne <pswoutlaw@gmail.com>
2017-11-25 14:46:10 -0600

    more

--- hello.go
+++ hello.go
@@ -1,29 +1,46 @@
 package main
 import (
-	"time"
-	/*"math/rand"*/
-	"fmt"
+	//"bytes"
+    "encoding/json"
+    "fmt"
+    "io/ioutil"
+	"net/http"
+	"strings"
 )
 var start = 0
 func main() {
-	c := make(chan int)
-	for i := 0; i < 5; i++ {
-		worker := &Worker{id: i}
-		go worker.process(c)
+	fmt.Println("Starting the application...")
+    response, err := http.Get("https://jsonplaceholder.typicode.com/posts/1")
+    if err != nil {
+        fmt.Printf("The HTTP request failed with error %s\n", err)
+    } else {
+        data, _ := ioutil.ReadAll(response.Body)
+		result := string(data)
+		dec := json.NewDecoder(strings.NewReader(result))
+		
+		type Message struct {
+			UserId int
+			Id int
+			Title string
+			Body string
+		}
+		var m Message
+		dec.Decode(&m)
+		fmt.Printf("%s: %d\n", m.Title, m.Id)
+		//dec := json.NewDecoder(strings.NewReader(data))
 	}
-	for {
-		start = start + 1
-		c <- start//rand.Int()
-		time.Sleep(time.Millisecond * 500)
-	}
-}
-type Worker struct {
-	id int
-}
-func (w *Worker) process(c chan int) {
-	for {
-		test := 66
-		data := <-c
-		fmt.Printf("worker %d got %d\n", w.id, data)
-	}
+	
+
+/*
+	fmt.Printf("posting to rest api")
+    jsonData := map[string]string{"firstname": "Nic", "lastname": "Raboy"}
+    jsonValue, _ := json.Marshal(jsonData)
+    response, err = http.Post("https://httpbin.org/post", "application/json", bytes.NewBuffer(jsonValue))
+    if err != nil {
+        fmt.Printf("The HTTP request failed with error %s\n", err)
+    } else {
+        data, _ := ioutil.ReadAll(response.Body)
+        fmt.Println(string(data))
+    }
+	fmt.Println("Terminating the application...")*/
 }
